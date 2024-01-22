433a88282c71857bbd039a46b0f80a2a6ad2db16
BenjaminGraziadei223946 <223946@buas.nl>
2024-01-22 16:55:00 +0100

    Update train_agent.py

--- 3. Reinforcement Learning/train_agent.py
+++ 3. Reinforcement Learning/train_agent.py
@@ -36,10 +36,10 @@ if __name__ == "__main__":
     task.execute_remotely(queue_name="default")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--learning_rate", type=float, default=0.005)
+    parser.add_argument("--learning_rate", type=float, default=0.0025)
     parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--n_steps", type=int, default=1000)
-    parser.add_argument("--n_epochs", type=int, default=20)
+    parser.add_argument("--n_steps", type=int, default=1024)
+    parser.add_argument("--n_epochs", type=int, default=80)
 
     args = parser.parse_args()
     main()
