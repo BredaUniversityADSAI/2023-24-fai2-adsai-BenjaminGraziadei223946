f55377a647c65c1104c6ca273e9e35a5bba70e4f
BenjaminGraziadei223946 <223946@buas.nl>
2024-01-22 15:02:15 +0100

    Update train_agent.py

--- 3. Reinforcement Learning/train_agent.py
+++ 3. Reinforcement Learning/train_agent.py
@@ -30,16 +30,16 @@ def main():
     model.save("ot2_model_agent007")
 
 if __name__ == "__main__":
-    task = Task.init(project_name='Mentor Group D/Group 1', task_name='agent 007')
+    task = Task.init(project_name='Mentor Group D/Group 1', task_name='agent 008')
     task.set_base_docker('deanis/2023y2b-rl:latest')
     #setting the task to run remotely on the default queue
     task.execute_remotely(queue_name="default")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--learning_rate", type=float, default=0.0005)
-    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--learning_rate", type=float, default=0.005)
+    parser.add_argument("--batch_size", type=int, default=128)
     parser.add_argument("--n_steps", type=int, default=2048)
-    parser.add_argument("--n_epochs", type=int, default=20)
+    parser.add_argument("--n_epochs", type=int, default=5)
 
     args = parser.parse_args()
     main()
